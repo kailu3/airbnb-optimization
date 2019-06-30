3db4d7c41b8fff699000b72ccbff8c8aa1d99ad5
Kai Lu <kai.lu@shopify.com>
2019-06-29 23:13:47 -0400

    Created cookiecutter project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An attempt to find the best airbnb listing under price constraints.',
+    author='kailu3',
+    license='MIT',
+)
